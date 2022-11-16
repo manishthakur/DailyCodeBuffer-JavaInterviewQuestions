c9c618cf3f3cb3e825b04000d55e2dcfa2f04ced
Manish Thakur <manishhakur.in@hotmail.com>
2022-11-16 16:48:33 -0500

    Initial code

---
+++ src/test/java/com/mt/DailyCodeBufferJavaInterviewQuestions/DailyCodeBufferJavaInterviewQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mt.DailyCodeBufferJavaInterviewQuestions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyCodeBufferJavaInterviewQuestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
